0e36b89dd340b6409aefa6ea9e292616ebf0a8e1
Sebastian Castellanos Sandoval <u1201755@unimilitar.edu.co>
2022-10-06 23:18:55 -0500

    Website to offer Carolina Spa services

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.swiper = new Swiper({
+    el: ".slider__box",
+    slideClass: "slider__slide",
+    createElements: true,
+    autoplay: {
+        delay: 5000
+    },
+    loop: true,
+    pagination: true,
+    navigation: true,
+});
